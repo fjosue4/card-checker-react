7896a000f5c920a7191051349002b6b4b87a6b09
Franklin Méndez <franklinjosue.net@gmail.com>
2022-10-04 19:53:32 -0600

    Dark theme

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import { CardModule } from './components/CardModule/CardModule';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { HowItWorks } from './components/HowItWorks/HowItWorks';
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("dark")
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  }
   return (
-    <div>
+    <ThemeContext.Provider value={{theme, setTheme}}>
+    <div className='App' id={theme}>
       <Header />
       <h1>Welcome to the card checker tool!</h1>
       <CardModule />
       <HowItWorks />
       <Footer />
     </div>
+    </ThemeContext.Provider>
   );
 }
 
