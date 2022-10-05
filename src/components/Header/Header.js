3d05c35a8fafd40b40809c2af90fa6055d834cc6
Franklin Méndez <franklinjosue.net@gmail.com>
2022-10-05 08:53:18 -0600

    Responsive and dark mode fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,7 +20,7 @@ export class Header extends React.Component {
             <a target='_blank' rel="noreferrer" href="https://github.com/fjosue4/credit-card-checker-react"><Icon icon="akar-icons:github-fill" /></a>
             <div className="theme-toggler" onClick={this.handleThemeToggle}>
             <Icon icon="charm:sun" id="sun-icon" className="active" />
-            <Icon icon="charm:moon" id="moon-icon" className="inactive" />
+            <Icon icon="charm:moon" id="moon-icon" className="active" />
         </div>
             </div>
         </header>)
